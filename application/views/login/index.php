dbcd279a73c71314d2efb7ee39d9458606e47aa4
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-17 00:49:04 +0800

    tukar link kepada butang

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -19,7 +19,8 @@ diatas($title);
 	<td align=center>
 		<br><img src="CoffeeHouse.png" width="250" height="175">
 		<br>Masukkan email & kata laluan.
-		<br><a class="btn btn-success" href="<?php echo URL ?>/login/masuk">Log Masuk</a>
+		<br><button type="button" class="btn btn-success" data-toggle="modal"
+		data-target=".login-sm">Log Masuk</button>
 		<hr>Tidak mempunyai akaun? Sila daftar di sini.
 		<br><a class="btn btn-success" href="<?php echo URL ?>/login/daftar">Daftar Akaun</a>
 	</td>
@@ -31,18 +32,31 @@ diatas($title);
 <!-- Extra large modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-xl">Extra large modal</button>
 
-<div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-	<div class="modal-dialog modal-xl" role="document">
-		<div class="modal-content">
-			...
-		</div>
-	</div><!-- / class="modal-dialog -->
+<div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog"
+aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-xl" role="document">
+<div class="modal-content">
+	<div class="modal-header">
+		<h4 class="modal-title">Tajuk</h4>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">&times;</span></button>
+	</div>
+	<div class="modal-body">
+		Tengah
+	</div><!-- / class="modal-body" -->
+	<div class="modal-footer">
+		Nota Kaki
+	</div><!-- / class="modal-footer" -->
+</div><!-- / class="modal-content" -->
+</div><!-- / class="modal-dialog" -->
 </div><!-- / class="modal fade -->
+
 <!-- ########################################################################################## -->
 <!-- Large modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
 
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog"
+aria-labelledby="myLargeModalLabel" aria-hidden="true">
 <div class="modal-dialog modal-lg" role="document">
 <div class="modal-content">
 	<div class="modal-header">
@@ -57,20 +71,31 @@ diatas($title);
 		Nota Kaki
 	</div><!-- / class="modal-footer" -->
 </div><!-- / class="modal-content" -->
-</div><!-- / class="modal-dialog -->
+</div><!-- / class="modal-dialog" -->
 </div><!-- / class="modal fade -->
 <!-- ########################################################################################## -->
 <!-- Small modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
+<!-- button type="button" class="btn btn-success" data-toggle="modal"
+data-target=".login-sm">Log Masuk</button -->
 
-<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-	<div class="modal-dialog modal-sm" role="document">
-		<div class="modal-content">
-			...
-		</div>
-	</div><!-- / class="modal-dialog -->
+<div class="modal fade login-sm" tabindex="-1" role="dialog"
+aria-labelledby="mySmallModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-sm" role="document">
+<div class="modal-content">
+	<div class="modal-header">
+		<h4 class="modal-title">Tajuk</h4>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">&times;</span></button>
+	</div>
+	<div class="modal-body">
+		Login
+	</div><!-- / class="modal-body" -->
+	<div class="modal-footer">
+		Nota Kaki
+	</div><!-- / class="modal-footer" -->
+</div><!-- / class="modal-content" -->
+</div><!-- / class="modal-dialog" -->
 </div><!-- / class="modal fade -->
-</div>
 <!-- ########################################################################################## -->
 <?php
 diJquery();
