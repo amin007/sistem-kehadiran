5c791aa21a0b410602d7add2ee38d3153b5003a3
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-17 00:48:45 +0800

    letak koding asas

--- application/views/zzz/index.php
+++ application/views/zzz/index.php
@@ -17,6 +17,72 @@ diatas($title);
 </div><!-- / class="card" -->
 </div><!-- / class="container" -->
 
+<!-- ########################################################################################## -->
+<!-- Extra large modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-xl">Extra large modal</button>
+
+<div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-xl" role="document">
+<div class="modal-content">
+	<div class="modal-header">
+		<h4 class="modal-title">Tajuk</h4>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">&times;</span></button>
+	</div>
+	<div class="modal-body">
+		Tengah
+	</div><!-- / class="modal-body" -->
+	<div class="modal-footer">
+		Nota Kaki
+	</div><!-- / class="modal-footer" -->
+</div><!-- / class="modal-content" -->
+</div><!-- / class="modal-dialog" -->
+</div><!-- / class="modal fade -->
+
+<!-- ########################################################################################## -->
+<!-- Large modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-lg" role="document">
+<div class="modal-content">
+	<div class="modal-header">
+		<h4 class="modal-title">Tajuk</h4>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">&times;</span></button>
+	</div>
+	<div class="modal-body">
+		Tengah
+	</div><!-- / class="modal-body" -->
+	<div class="modal-footer">
+		Nota Kaki
+	</div><!-- / class="modal-footer" -->
+</div><!-- / class="modal-content" -->
+</div><!-- / class="modal-dialog" -->
+</div><!-- / class="modal fade -->
+<!-- ########################################################################################## -->
+<!-- Small modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
+
+<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-sm" role="document">
+<div class="modal-content">
+	<div class="modal-header">
+		<h4 class="modal-title">Tajuk</h4>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">&times;</span></button>
+	</div>
+	<div class="modal-body">
+		Tengah
+	</div><!-- / class="modal-body" -->
+	<div class="modal-footer">
+		Nota Kaki
+	</div><!-- / class="modal-footer" -->
+</div><!-- / class="modal-content" -->
+</div><!-- / class="modal-dialog" -->
+</div><!-- / class="modal fade -->
+<!-- ########################################################################################## -->
+
 
 <?php
 diJquery();
