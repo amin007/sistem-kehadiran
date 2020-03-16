c7a65111520599a36504d5ce87c9753bd945e2b2
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-16 23:39:58 +0800

    tambah mvc untuk login

---
+++ application/controllers/logincontroller.php
@@ -0,0 +1,41 @@
+<?php
+
+class LoginController extends Controller
+{
+#--------------------------------------------------------------------------------------------------
+	protected $_tajukModulDaa = 'Halaman Login';
+#--------------------------------------------------------------------------------------------------
+	public function __construct($model, $action)
+	{
+		parent::__construct($model, $action);
+		$this->_setModel($model);
+	}
+#--------------------------------------------------------------------------------------------------
+	public function index()
+	{
+		//echo '<hr>Nama class ini :' . __METHOD__ . '()<hr>';
+		# mula baca database
+		try {
+			//$this->_setView('index');# nama fail di View
+			# Used to define the page title
+			$this->_view->set('title', $this->_tajukModulDaa);
+			$this->_view->set('tajukModul', 'Ini Dashboard Utama');
+			$this->_view->set('action', '&nbsp;...&nbsp;');
+			//*/
+			return $this->_view->output();
+		} catch (Exception $e) {
+			echo "Application error:" . $e->getMessage();
+		}
+	}
+#--------------------------------------------------------------------------------------------------
+	public function masuk()
+	{
+		echo '<hr>Nama class ini :' . __METHOD__ . '()<hr>';
+	}
+#--------------------------------------------------------------------------------------------------
+	public function daftar()
+	{
+		echo '<hr>Nama class ini :' . __METHOD__ . '()<hr>';
+	}
+#--------------------------------------------------------------------------------------------------
+}
