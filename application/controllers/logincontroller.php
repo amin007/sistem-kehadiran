326b71467bfac03de302f52b32e0460ea35d696e
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-19 22:27:05 +0800

    tambah function checkteacher() dan semakError()

--- application/controllers/logincontroller.php
+++ application/controllers/logincontroller.php
@@ -2,6 +2,7 @@
 
 class LoginController extends Controller
 {
+#==================================================================================================
 #--------------------------------------------------------------------------------------------------
 	protected $_tajukModulDaa = 'Halaman Login';
 #--------------------------------------------------------------------------------------------------
@@ -58,4 +59,60 @@ class LoginController extends Controller
 		echo '<hr>Nama class ini :' . __METHOD__ . '()<hr>';
 	}
 #--------------------------------------------------------------------------------------------------
+#==================================================================================================
+#--------------------------------------------------------------------------------------------------
+	public function checkteacher()
+	{
+		//echo '<hr>Nama class ini :' . __METHOD__ . '()<hr>';
+		#------------------------------------------------------------------------------------------
+		$teacher_emailid = '';
+		$teacher_password = '';
+		$error_teacher_emailid = '';
+		$error_teacher_password = '';
+		$error = 0;
+
+		if(empty($_POST["teacher_emailid"]))
+		{
+			$error_teacher_emailid = 'Email Address is required';
+			$error++;
+		}
+		else
+		{
+			$teacher_emailid = $_POST["teacher_emailid"];
+		}
+
+		if(empty($_POST["teacher_password"]))
+		{
+			$error_teacher_password = 'Password is required';
+			$error++;
+		}
+		else
+		{
+			$teacher_password = $_POST["teacher_password"];
+		}
+		#------------------------------------------------------------------------------------------
+		$this->semakError($error,$error_teacher_emailid,$error_teacher_password);
+		#------------------------------------------------------------------------------------------
+	}
+#--------------------------------------------------------------------------------------------------
+	function semakError($error,$error_teacher_emailid,$error_teacher_password)
+	{
+		if($error > 0)
+		{
+			$output = array(
+				'error'	=> true,
+				'error_teacher_emailid'	=> $error_teacher_emailid,
+				'error_teacher_password' => $error_teacher_password
+			);
+		}
+		else
+		{
+			$output = array('success' => true);
+		}
+
+		echo json_encode($output);
+		#
+	}
+#--------------------------------------------------------------------------------------------------
+#==================================================================================================
 }
