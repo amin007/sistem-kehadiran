e7fe1632f8998e3202ad06ee432fff775a4dcfda
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-19 21:31:12 +0800

    tukar pautan baru

--- application/views/home/index-template.php
+++ application/views/home/index-template.php
@@ -69,8 +69,8 @@ echo $link3[2] . "\r";
 
                             <div class="header-bar-menu">
                                 <ul class="flex justify-content-center align-items-center py-2 pt-md-0">
-                                    <li><a href="#">Register</a></li>
-                                    <li><a href="#">Login</a></li>
+                                    <li><a href="<?php echo URL ?>/login/teacher">Teacher</a></li>
+                                    <li><a href="<?php echo URL ?>/login/teacher">Admin</a></li>
                                 </ul>
                             </div><!-- .header-bar-menu -->
                         </div><!-- .col -->
