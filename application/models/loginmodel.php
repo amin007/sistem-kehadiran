943f25593497cae2ea0d0f441af91fe514e2e2f1
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-19 23:19:59 +0800

    bentuk beberapa function untuk capai database

--- application/models/loginmodel.php
+++ application/models/loginmodel.php
@@ -3,6 +3,31 @@
 class LoginModel extends Model
 {
 #--------------------------------------------------------------------------------------------------
+	public function semakDatabase($userEmailid,$userPassword)
+	{
+		$sql = $this->bentukSqlTeacher();
+		$userPassword = $this->dataSqlTeacher($sql);
+	}
+#--------------------------------------------------------------------------------------------------
+	function bentukSqlTeacher()
+	{
+		$sql = "SELECT * FROM tbl_teacher "
+		. "\r WHERE teacher_emailid = '" . $teacher_emailid . "' "
+		. "";
+
+		return $sql;
+	}
+#--------------------------------------------------------------------------------------------------
+	function dataSqlTeacher($sql)
+	{
+		$this->_setSql($sql);
+		$userPassword = $this->getAll();
+
+		if (empty($userPassword)){ return false; }
+
+		return $userPassword;		
+	}
+#--------------------------------------------------------------------------------------------------
 	public function getNews()
 	{
 		$this->_setSql($sql);
