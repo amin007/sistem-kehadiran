ff24d0b52c68f30d3677d60354b89ef83bb5d66b
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-20 02:24:10 +0800

    susun balik koding

--- application/views/home/index-template002.php
+++ application/views/home/index-template002.php
@@ -46,37 +46,43 @@ echo $link3[2] . "\r";
 <body>
 <div class="hero-content">
 <header class="site-header">
-            <div class="top-header-bar">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12 col-lg-6 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
-                            <div class="header-bar-email d-flex align-items-center">
-                                <i class="fa fa-envelope"></i><a href="#"><?php echo $email ?></a>
-                            </div><!-- .header-bar-email -->
+	<!-- ########################################################################################## -->
+	<div class="top-header-bar">
+	<div class="container-fluid">
+	<div class="row">
+		<!-- ////////////////////////////////////////////////////////////////////////////////////// -->
+		<div class="col-12 col-lg-6 d-none d-md-flex flex-wrap
+		justify-content-center justify-content-lg-start mb-3 mb-lg-0">
+			<div class="header-bar-email d-flex align-items-center">
+				<i class="fa fa-envelope"></i><a href="#"><?php echo $email ?></a>
+			</div><!-- .header-bar-email -->
 
-                            <div class="header-bar-text lg-flex align-items-center">
-                                <p><i class="fa fa-phone"></i><?php echo $telefon ?> </p>
-                            </div><!-- .header-bar-text -->
-                        </div><!-- .col -->
+			<div class="header-bar-text lg-flex align-items-center">
+				<p><i class="fa fa-phone"></i><?php echo $telefon ?> </p>
+			</div><!-- .header-bar-text -->
+		</div><!-- .col -->
+		<!-- ////////////////////////////////////////////////////////////////////////////////////// -->
+		<div class="col-12 col-lg-6 d-flex flex-wrap
+		justify-content-center justify-content-lg-end align-items-center">
+			<div class="header-bar-search">
+				<form class="flex align-items-stretch">
+				<input type="search" placeholder="What would you like to learn?">
+				<button type="submit" value="" class="flex justify-content-center align-items-center">
+					<i class="fa fa-search"></i></button>
+				</form>
+			</div><!-- .header-bar-search -->
 
-                        <div class="col-12 col-lg-6 d-flex flex-wrap justify-content-center justify-content-lg-end align-items-center">
-                            <div class="header-bar-search">
-                                <form class="flex align-items-stretch">
-                                    <input type="search" placeholder="What would you like to learn?">
-                                    <button type="submit" value="" class="flex justify-content-center align-items-center"><i class="fa fa-search"></i></button>
-                                </form>
-                            </div><!-- .header-bar-search -->
-
-                            <div class="header-bar-menu">
-                                <ul class="flex justify-content-center align-items-center py-2 pt-md-0">
-                                    <li><a href="<?php echo URL ?>/login/teacher">Teacher</a></li>
-                                    <li><a href="<?php echo URL ?>/login/admin">Admin</a></li>
-                                </ul>
-                            </div><!-- .header-bar-menu -->
-                        </div><!-- .col -->
-                    </div><!-- .row -->
-                </div><!-- .container-fluid -->
-            </div><!-- .top-header-bar -->
+			<div class="header-bar-menu">
+				<ul class="flex justify-content-center align-items-center py-2 pt-md-0">
+				<li><a href="<?php echo URL ?>/login/teacher">Teacher</a></li>
+				<li><a href="<?php echo URL ?>/login/admin">Admin</a></li>
+				</ul>
+			</div><!-- .header-bar-menu -->
+		</div><!-- .col -->
+		<!-- ////////////////////////////////////////////////////////////////////////////////////// -->
+	</div><!-- .row -->
+	</div><!-- .container-fluid -->
+	</div><!-- .top-header-bar -->
 	<!-- ########################################################################################## -->
 	<div class="nav-bar">
 	<div class="container">
