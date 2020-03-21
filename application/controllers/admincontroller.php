cdbbb7a4e560447ae4fccf70b3b7336488a76df6
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-22 01:14:29 +0800

    fungsi ini hanya untuk logout sahaja

--- application/controllers/admincontroller.php
+++ application/controllers/admincontroller.php
@@ -135,24 +135,10 @@ class AdminController extends Controller
 	public function logout()
 	{
 		//echo '<hr>Nama class ini :' . __METHOD__ . '()<hr>';
-		# mula baca database
-		try {
-			$this->_setView('index-admin');# nama fail di View
-			# Used to define the page title
-			$this->_view->set('title', $this->_tajukModulDaa);
-			$this->_view->set('tajukModul', 'Ini Dashboard Logout');
-			$this->_view->set('action', '&nbsp;...&nbsp;');
+		unset($_SESSION);
+		session_destroy();
 
-			return $this->_view->output();
-		} catch (Exception $e) {
-			$errors[] = $e->getMessage();
-			$_SESSION['message'] = $errors;
-			$_SESSION['type'] = 'error';
-
-			debugValue($_SESSION, '_SESSION');
-			//header('Location: ');
-			//exit;
-		}
+		header('location:' . URL . '');
 		//*/
 	}
 #--------------------------------------------------------------------------------------------------
