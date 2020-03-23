b0bef2af3a976e5875dd34803934db73dfb324ef
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-23 21:10:44 +0800

    tukar pembolehubah yang betul

--- system/core/function.php
+++ system/core/function.php
@@ -119,7 +119,7 @@ onclick="confirmdelete(007)">Delete</button>
 <script type="text/javascript">
 function confirmdelete(id)
 {
-	var messege="Are you sure to DELETE the record(id:"+id+")?";
+	var message="Are you sure to DELETE the record(id:"+id+")?";
 	var r=confirm(message);
 	if(r==true)
 	{
