5d4caa472c44a8bbfceac26c32cb8013e1554455
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-24 01:58:33 +0800

    tukar kepada alert-warning

--- application/views/admin/1grade.php
+++ application/views/admin/1grade.php
@@ -141,9 +141,10 @@ $(document).ready(function(){
 			data:{grade_id:grade_id, action:'delete'},
 			success:function(data)
 			{
-				$('#message_operation').html('<div class="alert alert-danger">'+data+'</div>');
+				$('#message_operation').html('<div class="alert alert-warning">'+data+'</div>');
 				$('#deleteModal').modal('hide');
 				dataTable.ajax.reload();
+				$('#formModal').modal('hide');
 			}
 		})
 	});
