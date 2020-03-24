85a929de882a7d4d8df6173261a75244993ab6c8
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-03-24 20:37:04 +0800

    tambah koding baru

--- application/views/admin/1grade.php
+++ application/views/admin/1grade.php
@@ -169,6 +169,22 @@ END;
 	function gradeTable002($url)
 	{
 		print <<<END
+	var t = $('#myTable').DataTable({
+	"ajax": "$url/admin/gradeAction",
+	searchHighlight: true,
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		"targets": 0
+	}],
+	"order": [[ 1, 'asc' ]]
+    });
+
+	t.on( 'order.dt search.dt', function (){
+		t.column(0, {search:'applied', order:'applied'}).nodes().
+		each( function (cell, i) {cell.innerHTML = i+1;});
+    }).draw();
+
 END;
 		#
 	}
